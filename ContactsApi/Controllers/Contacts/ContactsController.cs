32741c3972660ed4b54d77c9793bf12178953445
Matias Espinosa <matiaseespinosa98@gmail.com>
2023-08-20 16:23:53 -0300

    Feat: Se agrega CRUD de contactsApi con conexion a DB en memoria

---
+++ ContactsApi/Controllers/Contacts/ContactsController.cs
@@ -0,0 +1,70 @@
+﻿using ContactsApi.Models;
+using ContactsApi.Services.Contacts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApi.Controllers.Contacts
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactService _contactService;
+
+        public ContactsController(ContactService contactService) 
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+            var contacts = await _contactService.GetContacts();
+            if(contacts.Any())
+            {
+                return Ok(contacts);
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(Contact addContactRequest)
+        {
+            await _contactService.AddContact(addContactRequest);
+            return Ok(addContactRequest);
+        }
+        [HttpGet]
+        [Route("{email}")]
+        public async Task<IActionResult> GetContact([FromRoute] string email)
+        {
+            var contact = await _contactService.FindContact(email);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
+        }
+
+        [HttpPut]
+        [Route("{email}")]
+        public async Task<IActionResult> UpdateContact(Contact updateContactRequest)
+        {
+            if (await _contactService.UpdateContact(updateContactRequest))
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{email}")]
+        public async Task<IActionResult> DeleteContact(string email)
+        {
+            var task = await _contactService.DeleteContact(email);
+            if (task)
+            {                
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ ContactsApi/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsApi.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumberWork { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ProfileImage { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public string BirthDate { get; set; }
+
+    }
+}
